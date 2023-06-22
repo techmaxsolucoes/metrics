bfdcebed99f265363f2d75d07a6a08a42ba5b5bf
Maxwell Morais <mmorais@techmaxsolucoes.com.br>
2023-06-22 10:52:56 +0000

    Basic Metrics DocTypes

---
+++ metrics/metrics/doctype/metric/test_metric.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, TechMax Solucoes and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMetric(FrappeTestCase):
+	pass
---
+++ metrics/metrics/doctype/metric_category/metric_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, TechMax Solucoes and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Metric Category', {
+	// refresh: function(frm) {
+
+	// }
+});
